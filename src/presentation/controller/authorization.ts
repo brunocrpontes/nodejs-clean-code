81e55713495877000ab3835bf980d82930556891
Bruno Pontes <brunocrpontes.dev@gmail.com>
2020-07-08 00:27:48 -0300

    feat: ensure that AuthenticationController returns an error if name field isn't provided

--- src/presentation/controller/authorization.ts
+++ src/presentation/controller/authorization.ts
@@ -2,7 +2,8 @@
 export default class AuthorizationController {
   authorize (request: any): any {
     return {
-      statusCode: 422
+      statusCode: 422,
+      body: new Error('Há campos inválidos no formulário')
     }
   }
 }
