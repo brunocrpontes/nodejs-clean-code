e8bac28bffa8c9e3716494a7aa398310023e7c8c
Bruno Pontes <brunocrpontes.dev@gmail.com>
2020-07-08 00:18:58 -0300

    test: ensure AuthenticationController return 422 when name isn't provided

---
+++ src/presentation/controller/authorization.spec.ts
@@ -0,0 +1,17 @@
+import AuthorizationController from './authorization'
+
+describe('Authorization Controller', () => {
+  test("it should return status code 422 if name isn't provided", () => {
+    const sut = new AuthorizationController()
+    const request = {
+      body: {
+        email: 'brunocrpontes@gmail.com',
+        password: '********'
+      }
+    }
+
+    const response = sut.authorize(request)
+
+    expect(response.statusCode).toBe(422)
+  })
+})
