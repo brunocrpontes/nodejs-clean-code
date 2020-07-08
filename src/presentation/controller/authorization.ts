89872c1b3cc92d388eeb4a9b4b10f984f5eadd99
Bruno Pontes <brunocrpontes.dev@gmail.com>
2020-07-08 00:41:34 -0300

    feat: ensure that AuthenticationController returns an error if password field isn't provided

--- src/presentation/controller/authorization.ts
+++ src/presentation/controller/authorization.ts
@@ -1,9 +1,28 @@
 
 export default class AuthorizationController {
   authorize (request: any): any {
-    return {
-      statusCode: 422,
-      body: new Error('Há campos inválidos no formulário')
+    if (!request.body.email) {
+      return {
+        statusCode: 422,
+        body: {
+          message: 'Há campos inválidos no formulário',
+          errors: {
+            email: 'Campo Obrigatório.'
+          }
+        }
+      }
+    }
+
+    if (!request.body.password) {
+      return {
+        statusCode: 422,
+        body: {
+          message: 'Há campos inválidos no formulário',
+          errors: {
+            password: 'Campo Obrigatório.'
+          }
+        }
+      }
     }
   }
 }
